37b0d3e0e8ba080e8704498f6b5db5bb5eabe85b
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2025-05-26 18:11:20 -0300

    exercicios das aulas de tipos primitivos

---
+++ src/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 30;
+        long numeroGrande = 300000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 30;
+        short idadeShort = 30;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caracter = 30;
+
+        System.out.println(verdadeiro);
+        System.out.println(idadeByte);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+        System.out.println(idadeShort);
+        System.out.println(salarioDouble);
+        System.out.println(falso);
+        System.out.println(caracter);
+        System.out.println(numeroGrande);
+        System.out.println(age);
+    }
+}
---
+++ src/devdojo/maratonajava/introducao/ExercicioTiposPrimitivos.java
@@ -0,0 +1,15 @@
+package devdojo.maratonajava.introducao;
+
+public class ExercicioTiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Dudu";
+        int idade = 5;
+        String endereco = "Guarulhos";
+        double salario = 1.250;
+        String dataSalario = "26/05/2019";
+
+        System.out.println("Eu " + nome + ", morando " + endereco + " confirmo que recebi o salario de " + salario + ", na data de " + dataSalario + '.');
+
+
+    }
+}
