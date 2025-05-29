1bd45c73dd06b63756e6d91df0803d0f7ef77d2b
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2025-05-29 12:22:34 -0300

    Aula: Estruturas de repeticao, (while, do-while e for)

--- src/devdojo/maratonajava/introducao/EstruturasDeRepeticao01.java
+++ src/devdojo/maratonajava/introducao/EstruturasDeRepeticao01.java
@@ -2,6 +2,16 @@ package devdojo.maratonajava.introducao;
 
 public class EstruturasDeRepeticao01 {
     public static void main(String[] args) {
-        
+        int count = 0;
+
+        while (count < 10 ){
+            count++;
+            System.out.println(count);
+        }
+
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("For" + i);
+        }
     }
 }
