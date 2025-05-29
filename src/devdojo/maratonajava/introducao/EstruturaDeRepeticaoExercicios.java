750c2c8032ae07d7b3f1fe3d5e48c16d863ac2dc
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2025-05-29 17:13:03 -0300

    Exercicio de estrutura de repeticao - for

---
+++ src/devdojo/maratonajava/introducao/EstruturaDeRepeticaoExercicios.java
@@ -0,0 +1,11 @@
+package devdojo.maratonajava.introducao;
+
+public class EstruturaDeRepeticaoExercicios {
+    public static void main(String[] args){
+        for(int contador= 0 ; contador <= 1000000 ; contador++) {
+            if (contador % 2 == 0){
+                System.out.println(contador);
+            }
+        }
+    }
+}
