3862b560090c67d05181cb6ecf0e449c2da23890
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2025-05-29 17:47:44 -0300

    alteracao apos acompanhar a correcao do exercicio

--- src/devdojo/maratonajava/introducao/ExerciciosEstruturasDeRepeticaoBreak.java
+++ src/devdojo/maratonajava/introducao/ExerciciosEstruturasDeRepeticaoBreak.java
@@ -1,4 +1,15 @@
 package devdojo.maratonajava.introducao;
 
 public class ExerciciosEstruturasDeRepeticaoBreak {
+    public static void main(String[] args) {
+        double carroValor = 8000;
+        double valorParcela ;
+
+        for (int i = 1 ; i <= carroValor ; i++){
+            valorParcela = carroValor / i;
+            if( valorParcela < 1000){
+                System.out.println("Parcela de " + i + "x" + " de " + valorParcela);
+            }
+        }
+    }
 }
