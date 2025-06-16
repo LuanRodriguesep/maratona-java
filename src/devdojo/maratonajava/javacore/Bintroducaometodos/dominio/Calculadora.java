fff3bfc7780d0c11bf469593e8ca8ddc70483942
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2025-06-16 15:22:35 -0300

    Criando metodos com parametros, e tratando os dados com if

--- src/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -14,6 +14,9 @@ public class Calculadora {
     }
 
     public double dividiDoisNumeros(double num1, double num2) {
-       return num1 / num2;
+        if (num2 != 0){
+            return num1 / num2;
+        }
+        return 0;
     }
 }
